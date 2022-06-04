d244943d0da9cf48a09675d66e98d8c810e60a1a
dhstambolliu <stambolliudhimiter@gmail.com>
2022-06-04 16:41:03 +0200

    new commit 04.06.2022

---
+++ src/app/components/promotion-list/promotion-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PromotionListComponent } from './promotion-list.component';
+
+describe('PromotionListComponent', () => {
+  let component: PromotionListComponent;
+  let fixture: ComponentFixture<PromotionListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PromotionListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PromotionListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/promotion-list/promotion-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {Packages} from "../carousel/carousel.component";
+import {HttpErrorResponse} from "@angular/common/http";
+import {PackagesService} from "../services/packages.service";
+
+@Component({
+  selector: 'app-promotion-list',
+  templateUrl: './promotion-list.component.html',
+  styleUrls: ['./promotion-list.component.scss']
+})
+export class PromotionListComponent implements OnInit {
+  packages: Packages[] | any;
+  constructor(private packagesService: PackagesService) { }
+
+  ngOnInit(): void {
+    this.getPromotionalOffers();
+  }
+
+  public getPromotionalOffers(): void {
+    this.packagesService.getPromotionalOffers().subscribe(
+      (response: Packages[]) => {
+        this.packages = response;
+        console.log(this.packages);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+}
