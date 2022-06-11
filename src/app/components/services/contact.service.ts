cd07b9ed2a2a8936b0b477cbeec783c94637eb3c
dhstambolliu <stambolliudhimiter@gmail.com>
2022-06-11 16:04:10 +0200

    added new features

---
+++ src/app/components/services/contact.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor() { }
+
+  addContact(value: any) {
+    
+  }
+}
--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Contact} from "../../models/models";
+import {AddNewPackagesService} from "../../components/services/add-new-packages.service";
+import {ContactService} from "../../components/services/contact.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-us',
@@ -9,15 +12,20 @@ import {Contact} from "../../models/models";
 export class ContactUsComponent implements OnInit {
   model: Contact = new Contact();
 
-  constructor() {
+  constructor(public contactService: ContactService) {
   }
+
+  contactForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required])
+  })
 
   ngOnInit(): void {
   }
 
-  registerForm(): void  {
+  registerForm(): void {
 
-    console.info(this.model)
 
   }
 }
