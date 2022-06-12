b4c04549306f4bb70b5a1845c465dd241903627a
dhstambolliu <stambolliudhimiter@gmail.com>
2022-06-12 11:30:44 +0200

    added new features

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Contact} from "../../models/models";
 import {ContactService} from "../../components/services/contact.service";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MessageService} from "primeng/api";
 
 @Component({
   selector: 'app-contact-us',
@@ -11,7 +12,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 export class ContactUsComponent implements OnInit {
   model: Contact = new Contact();
 
-  constructor(private _formBuilder: FormBuilder, public contactService: ContactService) {
+  constructor(private _formBuilder: FormBuilder, public contactService: ContactService, private messageService: MessageService) {
   }
 
   contactForm = new FormGroup({
@@ -23,18 +24,23 @@ export class ContactUsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public checkError = (controlName: string, errorName: string) => {
+    // @ts-ignore
+    return this.packageForm.controls[controlName].hasError(errorName);
+  }
+
   registerForm(): void {
     this.contactService.addContact(this.model as Contact)
       .subscribe((response: any) => {
         if (response.success) {
-          alert("Message sent!");
+          this.messageService.add({severity:'success', summary:'Sending form was success', detail:"Message is sent successfully. You will be contacted very soon!"});
           this.clearContactForm();
         } else {
-          alert(response.messages.join(", "))
+          this.messageService.add({severity:'warning', summary:'Contact form errors', detail: response.messages ? response.messages.join(", ") : "Unknown error"});
         }
       }, error => {
         console.error(error);
-        alert(error.message);
+        this.messageService.add({severity:'warning', summary:'Contact form errors', detail: error && error.message ? error.message : "Unknown error"});
       });
   }
 
